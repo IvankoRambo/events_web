f8ff01a2bba87bc4ddecb989a0d1b2351b8e445a
Ivanko <ivankorambo@gmail.com>
2015-06-05 19:29:46 +0300

    V2

---
+++ logging_db2/header.php
@@ -0,0 +1,5 @@
+<head>
+	<title>Site page</title>
+	<link rel="stylesheet" type="text/css" src="css/bootstrap.min.css">
+	<script type="application/javascript" src="js/bootstrap.min.js"></script>
+</head>
